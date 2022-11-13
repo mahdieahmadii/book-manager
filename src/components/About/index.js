d2a41c4042769fe03b7d4fd44f3894003cb48cdb
mahdie <mahdie.ahmadii97@gmail.com>
2022-11-13 11:23:12 +0330

    feat: add route & routes

---
+++ src/components/About/index.js
@@ -0,0 +1,7 @@
+export const About = () => {
+    return <>
+        <div style={{ padding: "1rem 0" }}>
+            <h2>تمرین آموزشی react router</h2>
+        </div>
+    </>
+}
---
+++ src/components/Books/index.js
@@ -0,0 +1,7 @@
+export const Books = () => {
+    return <>
+        <div style={{ padding: "1rem 0" }} >
+            <h2>صفحه کتاب های ما</h2>
+        </div>
+    </>
+}
