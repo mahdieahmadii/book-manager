cc553754eeaf842c4b13a044f73a941177ac5b3d
mahdie <mahdie.ahmadii97@gmail.com>
2022-11-13 13:45:13 +0330

    feat: add search component

--- src/components/Books/index.js
+++ src/components/Books/index.js
@@ -1,7 +1,41 @@
+import { NavLink, Outlet, useSearchParams, useLocation } from "react-router-dom";
+import { getBooks } from "../../data/data";
+
+
 export const Books = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const location = useLocation();
+    const books = getBooks()
     return <>
-        <div style={{ padding: "1rem 0" }} >
-            <h2>صفحه کتاب های ما</h2>
+        <div style={{ display: "flex" }} >
+            <nav style={{ borderLeft: "solid 1px", padding: "1rem" }}>
+                <input
+                    value={searchParams.get("filter") || ""}
+                    onChange={event => {
+                        let filter = event.target.value;
+                        filter ? setSearchParams({ filter }) : setSearchParams({})
+                    }}
+                    type="text" placeholder="جستجوی کتاب" />
+                {
+                    books.filter((book) => {
+                        let filter = searchParams.get("filter");
+                        if (!filter) {
+                            return true;
+                        } else {
+                            let name = book.name.toLowerCase();
+                            return name.startsWith(filter.toLowerCase())
+                        }
+                    })
+                        .map(book => <NavLink style={({ isActive }) => {
+                            return {
+                                display: "block",
+                                margin: "1rem 0",
+                                color: isActive ? "red" : ""
+                            }
+                        }} to={`/books/${book.number}${location.search}`} key={book.number}>{book.name}</NavLink>)
+                }
+            </nav>
+            <Outlet />
         </div>
     </>
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Books } from './components/Books';
 import { About } from './components/About';
+import { Book } from './components/Book';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +13,22 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />}>
-          <Route path='/books' element={<Books />} />
+          <Route path='/books' element={<Books />} >
+            <Route
+              index
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>کتاب مورد نظر خود را انتخاب نمایید</p>
+                </main>
+              } />
+            <Route path=':bookId' element={<Book />} />
+          </Route>
           <Route path='/about' element={<About />} />
+          <Route path="*" element={
+            <main style={{ padding: "1rem" }}>
+              <p style={{ textAlign: "center" }}>آیتم مورد نظر یافت نشد</p>
+            </main>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
